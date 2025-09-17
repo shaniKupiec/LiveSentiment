0ef79a77cb24ebd6d897a49e6366400c21ba9c37
shani kupiec <shani.kupiec@gmail.com>
2025-09-17 21:17:11 +0300

    fix Error while Stopping

--- backend/obj/Debug/net8.0/LiveSentiment.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/LiveSentiment.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LiveSentiment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bdb16daf4f4c8b8f6c0a69c11f44d15ed1989d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39ba625d64d7792206a4effdd722af500e575c99")]
 [assembly: System.Reflection.AssemblyProductAttribute("LiveSentiment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LiveSentiment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
